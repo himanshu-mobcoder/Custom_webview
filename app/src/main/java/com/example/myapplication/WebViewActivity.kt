d61cce5418a154cccfa0311ed668f1f55a6a046a
Priya kushwah <himanshu.gupta@mobcoder.com>
2023-06-01 10:25:13 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/WebViewActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.myapplication.databinding.ActivityWebviewBinding
+
+class WebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebviewBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val url = intent.getStringExtra("URL")
+        binding.customWebview.settings.javaScriptEnabled = true
+
+        binding.customWebview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        if (url != null) {
+            binding.customWebview.loadUrl(url)
+        }
+    }
+}
